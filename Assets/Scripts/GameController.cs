3c73760bb9b9f4e4f5213066560c5d8a219dd25a
rcarrash <rcarrash@yahoo.com.mx>
2015-02-28 17:02:09 +0000

    Game Controller Script update
    Includes instatiate of ground tiles

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,15 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class GameController : MonoBehaviour
+{
 	
+	public int width = 19;
+	public int height = 19;
+	public GameObject floorTile;
+	public GameObject wallTile;
+	
+	void Awake ()
+	{
+		PaintMyGridYall ();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
+		
 	}
+	
+	public void PaintMyGridYall ()
+	{
+		// 9x9
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				GameObject tile = Instantiate (floorTile, new Vector2 (x - (width / 2), y - (height / 2)), Quaternion.identity) as GameObject;
+			}
+		}
+	}
+	
+	private void CreateWall ()
+	{
+		
+	}
+	
 }

2fd02c603d1c4638333989b83169df0ea4d398ad
Ramzawulf <rene.zurita.sanchez@gmail.com>
2015-02-28 20:47:31 +0000

    Bull Logic

--- Assets/Scripts/Bull.cs
+++ Assets/Scripts/Bull.cs
@@ -3,52 +3,82 @@ using System.Collections;
 
 public class Bull : MonoBehaviour
 {
+	[SerializeField]
+	public enum BullDirection
+	{
+		Up, 
+		Down,
+		Left,
+		Right
+	}
 	
 	//Moves per second
-	public float movementSpeedBull = 1;
-	private float lastMove = 0;
+	public float movementSpeed = 1;
+	public BullDirection direction;
+	private float lastMovement;
 	
 	
 	void Start ()
 	{
-		//lastMove = Time.time;
+		direction = BullDirection.Up;
+		lastMovement = Time.time;
 	}
 	
 	void Update ()
 	{
-		//Move ();
+		Move ();
 	}
 	
 	void Move ()
 	{
+		if (Time.time < (lastMovement + (1 / movementSpeed)))
+			return;
+					
+		Vector2 targetPosition = transform.position;
 
-		//int horizontal = (int)Input.GetAxisRaw ("Horizontal");
-		//int vertical = (int)Input.GetAxisRaw ("Vertical");
-		
-		//if (Time.time < (lastMove + (1 / movementSpeed)))
-			//return;
-		
-		//if (horizontal != 0) {
-			//Vector2 targetposition = new Vector2 (transform.position.x + horizontal, transform.position.y);
-			//rigidbody2D.MovePosition (targetposition);
-			//transform.rotation = Quaternion.identity;
-		//} //else if (vertical != 0) {
-			//Vector2 targetposition = new Vector2 (transform.position.x, transform.position.y + vertical);
-			//rigidbody2D.MovePosition (targetposition);
-			//transform.rotation = Quaternion.identity;
-		//}
-		//lastMove = Time.time;
-		
+		SetDirection ();
+
+		switch (direction) {
+		case BullDirection.Up:
+			targetPosition = new Vector2 (transform.position.x, transform.position.y + 1);
+			break;
+		case BullDirection.Down:
+			targetPosition = new Vector2 (transform.position.x, transform.position.y - 1);
+			break;
+		case BullDirection.Left:
+			targetPosition = new Vector2 (transform.position.x - 1, transform.position.y);
+			break;
+		case BullDirection.Right:
+			targetPosition = new Vector2 (transform.position.x + 1, transform.position.y);
+			break;
+		}
+
+		lastMovement = Time.time;
+		rigidbody2D.MovePosition (targetPosition);
 	}
-	
+
+	private void SetDirection ()
+	{
+		int horizontal = (int)Input.GetAxisRaw ("BullHorizontal");
+		int vertical = (int)Input.GetAxisRaw ("BullVertical");
+
+		if (horizontal > 0 && direction != BullDirection.Left) {
+			direction = BullDirection.Right;
+		} else if (horizontal < 0 && direction != BullDirection.Right) {
+			direction = BullDirection.Left;
+		} else if (vertical > 0 && direction != BullDirection.Down) {
+			direction = BullDirection.Up;
+		} else if (vertical < 0 && direction != BullDirection.Up) {
+			direction = BullDirection.Down;
+		}
+	}
+
 	void OnTriggerEnter (Collider other)
 	{
-		print ("trigger");
 	}
 	
 	void OnCollisionEnter2D (Collision2D coll)
 	{
-		print ("Choqué");
 		
 	}
 }

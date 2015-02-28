bf6d912f510219eb3455e6dc02bd2dffde297974
Ramzawulf <rene.zurita.sanchez@gmail.com>
2015-02-28 18:33:47 +0000

    player
    Creation of the Player Gameobject, and its script.

---
+++ Assets/Scripts/Farmer.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Farmer : MonoBehaviour
+{
+
+			//Moves per second
+			public float movementSpeed = 1;
+			private float lastMove = 0;
+
+
+			void Start ()
+			{
+						lastMove = Time.time;
+			}
+
+			void Update ()
+			{
+						Move ();
+			}
+
+			void Move ()
+			{
+						int horizontal = (int)Input.GetAxisRaw ("Horizontal");
+						int vertical = (int)Input.GetAxisRaw ("Vertical");
+
+						if (Time.time < (lastMove + (1 / movementSpeed)))
+									return;
+			
+						if (horizontal != 0) {
+									Vector2 targetposition = new Vector2 (transform.position.x + horizontal, transform.position.y);
+									rigidbody2D.MovePosition (targetposition);
+									transform.rotation = Quaternion.identity;
+						} else if (vertical != 0) {
+									Vector2 targetposition = new Vector2 (transform.position.x, transform.position.y + vertical);
+									rigidbody2D.MovePosition (targetposition);
+									transform.rotation = Quaternion.identity;
+						}
+						lastMove = Time.time;
+						
+			}
+
+			void OnTriggerEnter (Collider other)
+			{
+						print ("trigger");
+			}
+
+			void OnCollisionEnter2D (Collision2D coll)
+			{
+						print ("Choqué");
+						
+			}
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,34 +4,42 @@ using System.Collections;
 public class GameController : MonoBehaviour
 {
 	
-	public int width = 19;
-	public int height = 19;
-	public GameObject floorTile;
-	public GameObject wallTile;
+			public int width = 19;
+			public int height = 19;
+			public GameObject floorTile;
+			public GameObject wallTile;
 	
-	void Awake ()
-	{
-		PaintMyGridYall ();
-	}
-	
-	void Update ()
-	{
-		
-	}
-	
-	public void PaintMyGridYall ()
-	{
-		// 9x9
-		for (int x = 0; x < width; x++) {
-			for (int y = 0; y < height; y++) {
-				GameObject tile = Instantiate (floorTile, new Vector2 (x - (width / 2), y - (height / 2)), Quaternion.identity) as GameObject;
+			void Awake ()
+			{
+						PaintMyGridYall ();
 			}
-		}
-	}
 	
-	private void CreateWall ()
-	{
+			void Update ()
+			{
 		
-	}
+			}
+	
+			public void PaintMyGridYall ()
+			{
+						for (int x = 0; x < width; x++) {
+									for (int y = 0; y < height; y++) {
+												GameObject tile = Instantiate (floorTile, new Vector2 (x - (width / 2), y - (height / 2)), Quaternion.identity) as GameObject;
+									}
+						}
+						CreateWall ();
+			}
+	
+			private void CreateWall ()
+			{
+						//Upper & Lower border
+						for (int x = -1; x < width+1; x++) {
+									Instantiate (wallTile, new Vector2 (x - (width / 2), -(height / 2) - 1), Quaternion.identity);
+									Instantiate (wallTile, new Vector2 (x - (width / 2), (height / 2) + 1), Quaternion.identity);
+						}
+						for (int y = 0; y < height; y++) {
+									Instantiate (wallTile, new Vector2 ((width / 2) + 1, y - (height / 2)), Quaternion.identity);
+									Instantiate (wallTile, new Vector2 (-(width / 2) - 1, y - (height / 2)), Quaternion.identity);
+						}
+			}
 	
 }
